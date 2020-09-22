ae0cf8d1048b1bda33e225d983bf954c4d12ac79
Sergiy Lavryk <jagermesh@gmail.com>
2020-09-23 02:34:15 +0300

    test script

--- test.js
+++ test.js
@@ -1,4 +1,36 @@
-const ProgressLogger = require(__dirname + '/index.js');
+const MonitoringHub       = require('monitoring-hub');
+const MonitoringDashboard = require('monitoring-dashboard');
+const MonitoringSensor    = require('monitoring-sensor');
+const ProgressLogger      = require(__dirname + '/index.js');
+
+const config = {
+  hub: {
+    listenSensorsOnPort: 8082
+  , listenWebOnPort:     8081
+  }
+, dashboard: {
+    listenOnPort:        8083
+  , hubUrl: 'http://localhost:8081'
+  }
+, sensor: {
+    hubUrl: 'http://localhost:8082'
+  , metrics: [
+       { name: 'LA'
+       }
+     , { name: 'ProcessList'
+       }
+    ]
+  }
+};
+
+const hub = new MonitoringHub(config.hub);
+const dashboard = new MonitoringDashboard(config.dashboard);
+const sensor = new MonitoringSensor(config.sensor);
+
+hub.start();
+dashboard.start();
+sensor.start();
+
 
 function sleep(delay) {
   return new Promise(function(resolve) {
@@ -7,12 +39,12 @@ function sleep(delay) {
 }
 
 (async function() {
-  const op1max = 200;
+  const op1max = 20000;
   const op2max = 120;
   const timeout = 100;
   const progressLogger = new ProgressLogger();
-  const loggerSession1 = progressLogger.createSession('Testing progress from JS (1)');
-  const loggerSession2 = progressLogger.createSession('Testing progress from JS (2)');
+  const loggerSession1 = progressLogger.createSession('Testing progress (1)');
+  const loggerSession2 = progressLogger.createSession('Testing progress (2)');
   const loggerOperation11 = loggerSession1.createOperation('Main task (1)');
   const loggerOperation12 = loggerSession1.createOperation('Sub task (1)');
   const loggerOperation21 = loggerSession2.createOperation('Main task (2)');
@@ -27,14 +59,10 @@ function sleep(delay) {
       loggerOperation22.step();
       await sleep(timeout);
     }
-    // loggerOperation12.finish(false);
-    // loggerOperation22.finish(false);
     loggerOperation11.step();
     loggerOperation21.step();
     await sleep(timeout);
   }
-  // loggerOperation11.finish(false);
-  // loggerOperation22.finish(false);
   loggerSession1.finish();
   loggerSession2.finish();
 })();
